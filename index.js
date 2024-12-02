b8cdfc7eff4e588c9009347ff7131e0d4c8dedbb
Your Name <you@example.com>
2024-12-02 20:09:05 +0700

    tn

---
+++ index.js
@@ -0,0 +1,12 @@
+// const express = require("express")
+// const app = express()
+
+// app.get ("/", (req, res) => {
+//     res.send("Tony")
+// })
+// app.listen(3333, () => {
+//     console.log("Server 0n 3333")
+
+// })
+
+console.log("hello")
